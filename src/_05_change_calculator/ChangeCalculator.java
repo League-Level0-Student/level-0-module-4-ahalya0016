9479726cf9d5d2588013d3bfaf350010927a9401
League Student <league@iMac-21.attlocal.net>
2019-08-04 11:28:39 -0700

    Module 4 - Change Calculator

--- src/_05_change_calculator/ChangeCalculator.java
+++ src/_05_change_calculator/ChangeCalculator.java
@@ -4,6 +4,7 @@ package _05_change_calculator;
 
 
 import javax.swing.JOptionPane;
+import javax.swing.plaf.OptionPaneUI;
 
 /*
  * I have a pocket full of change. I hate doing math. Make me a program that
@@ -15,13 +16,15 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+		String nickels = JOptionPane.showInputDialog("How many nickels do you have?");
 		// Convert their answer to an int using Integer.parseInt()
-
+		int i = Integer.parseInt(nickels);
 		// Ask the user how many dimes they have, and convert their answer
-
+		String dimes = JOptionPane.showInputDialog("How many dimes do you have?");
+		Integer.parseInt(dimes);
 		// Ask the user how many quarters they have, and convert their answer
-
+		String quarters = JOptionPane.showInputDialog("How many quarters do you have?");
+		Integer.parseInt(quarters);
 		// Calculate how much money the user has and save it in a double variable 
 
 		// Tell the user how much money they have
